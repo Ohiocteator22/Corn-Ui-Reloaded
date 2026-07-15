299223e8beb26a7fbf448ca5efe8585132cfd975
∆LLURΞ.ÐΞV <134415771+Ohiocteator22@users.noreply.github.com>
2026-07-15 18:11:08 +0530

    Create Corn Ui loader.lua

---
+++ Loader/Corn Ui loader.lua
@@ -0,0 +1,27 @@
+function Nullscape()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Ohiocteator22/Corn-Ui-Reloaded/refs/heads/main/Scripts/Nullscape.lua"))()
+end
+
+function LOS()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Ohiocteator22/Corn-Ui-Reloaded/refs/heads/main/Scripts/Legends%20of%20speed.lua"))()
+end
+
+function Area51()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Ohiocteator22/Corn-Ui-Reloaded/refs/heads/main/Scripts/Area%2051.lua"))()
+end
+
+--functions only--
+if game.PlaceId == 3101667897 then
+  LOS()
+end
+
+if game.PlaceId == 100588763114828 or game.PlaceId == 129279692364812 then
+  Nullscape()
+end
+
+if game.PlaceId == 2092166489 then
+  Area51()
+end
+
+
+
