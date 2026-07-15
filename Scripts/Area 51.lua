0dd94fea8341cd6cb1d615c598af47a7eba12fcf
∆LLURΞ.ÐΞV <134415771+Ohiocteator22@users.noreply.github.com>
2026-07-15 18:06:09 +0530

    Update Area 51.lua

--- Scripts/Area 51.lua
+++ Scripts/Area 51.lua
@@ -1,6 +1,16 @@
 if game.PlaceId == 2092166489 then
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-    local Window = OrionLib:MakeWindow({Name = "Corn Hub 🥀", HidePremium = true, SaveConfig = true, ConfigFolder = "Corn3yConfig"})
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ohiocteator22/CornUi_Mobile-PC-compatible/refs/heads/main/CornUi.lua"))()
+    local Window = Library:CreateWindow({
+    Name = "Corn Hub",
+    Subtitle = "By Lifeless",
+    Icon = 80406291512141, -- or nil if this one also gets flagged
+    Theme = "Dark",
+    Intro = {
+        Image = 80406291512141,
+        Text = "By Lifeless",
+        Duration = 1.4,
+    },
+})
     --done making window--
     local player = game.Players.LocalPlayer   
     --done adding variables--
@@ -37,141 +47,50 @@ if game.PlaceId == 2092166489 then
     end
 
     --functions only--
+    Window:Notify({
+    Title = "Success",
+    Content = "CornUI Loaded!",
+    Duration = 4,
+    Type = "success"
+    })
     --making Auto get weapon tab--
-    local WeaponTab = Window:MakeTab({
-    Name = "Get Guns 💥",
-    Icon = "nil",
-    PremiumOnly = false
+    local Main = Window:CreateTab("Get Guns 💥", {
+    Icon = 98667413760537
     })
     
-    local Section = WeaponTab:AddSection({
-    Name = "Get all Guns"
+    local Section = Main:CreateSection("Get Guns")
+    Section:CreateDropdown({
+    Name = "Weapon",
+    Options = {
+        "AK47",
+        "M4A1",
+        "AWP",
+        "Colt Anaconda",
+        "SVD",
+        "RayGun",
+        "R870",
+        "P90",
+        "MP5K",
+        "Flamethrower",
+        "Desert Eagle",
+        "DB Shotgun",
+        "M16A2/M203",
+        "M14",
+        "M1014",
+        "G36C"
+    },
+    Default = "AK47",
+    Callback = function(name)
+        TeleportToWeapon(name)
+    end
     })
     
-    WeaponTab:AddButton({
-    Name = "Get Ak-47",
+    Main:CreateButton({
+    Name = "goto ak47",
     Callback = function()
         TeleportToWeapon("AK-47")
     end
     })
-    WeaponTab:AddButton({
-    Name = "Get An-94",
-    Callback = function()
-        TeleportToWeapon("AN-94")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get Colt Anaconda",
-    Callback = function()
-        TeleportToWeapon("Colt Anaconda")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get AWP",
-    Callback = function()
-        TeleportToWeapon("AWP")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get DB shotgun",
-    Callback = function()
-        TeleportToWeapon("DB Shotgun")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get Desert Eagle",
-    Callback = function()
-        TeleportToWeapon("Desert Eagle")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get Flamethrower",
-    Callback = function()
-        TeleportToWeapon("Flamethrower")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get G36C",
-    Callback = function()
-        TeleportToWeapon("G36C")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get M1014",
-    Callback = function()
-        TeleportToWeapon("M1014")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get M14",
-    Callback = function()
-        TeleportToWeapon("M14")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get M16A2/M203",
-    Callback = function()
-        TeleportToWeapon("M16A2/M203")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get M4A1",
-    Callback = function()
-        TeleportToWeapon("M4A1")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get MP5k",
-    Callback = function()
-        TeleportToWeapon("MP5K")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get P90",
-    Callback = function()
-        TeleportToWeapon("P90")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get R870",
-    Callback = function()
-        TeleportToWeapon("R870")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get RayGun",
-    Callback = function()
-        TeleportToWeapon("RayGun")
-    end
-    })
-
-    WeaponTab:AddButton({
-    Name = "Get SVD",
-    Callback = function()
-        TeleportToWeapon("SVD")
-    end
-    })
-
-    --WeaponTab:AddButton({
-    --Name = "Back to spawn",
-    --Callback = function()
-        
-    --end
-    --})
 end
 
 
