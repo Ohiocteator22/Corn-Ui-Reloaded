32b9843b74b342bb739da49583ba395409c1ced6
∆LLURΞ.ÐΞV <134415771+Ohiocteator22@users.noreply.github.com>
2026-08-08 13:56:57 +0530

    Create Init.lua

---
+++ Sourse/Widgets/Init.lua
@@ -0,0 +1,33 @@
+-- ============================================================
+-- CORNUI WIDGET REGISTRY
+-- ============================================================
+
+local Widgets = {}
+
+
+Widgets.Button =
+	require(script.Button)
+
+
+Widgets.Toggle =
+	require(script.Toggle)
+
+
+Widgets.Slider =
+	require(script.Slider)
+
+
+Widgets.Dropdown =
+	require(script.Dropdown)
+
+
+Widgets.ColorPicker =
+	require(script.ColorPicker)
+
+
+Widgets.Keybind =
+	require(script.Keybind)
+
+
+
+return Widgets
