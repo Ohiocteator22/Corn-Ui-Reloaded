98f7b031531d3d4f75c8f444dbc71134bda5029e
∆LLURΞ.ÐΞV <134415771+Ohiocteator22@users.noreply.github.com>
2026-08-09 12:25:06 +0530

    Create Image.lua

---
+++ Sourse/Widgets/Image.lua
@@ -0,0 +1,359 @@
+-- ============================================================
+-- CORNUI IMAGE WIDGET
+-- ============================================================
+
+
+local Utils = require(
+	script.Parent.Parent.Core.Utils
+)
+
+local State = require(
+	script.Parent.Parent.Core.State
+)
+
+
+
+local Image = {}
+
+Image.__index = Image
+
+
+
+
+-- ============================================================
+-- CREATE
+-- ============================================================
+
+function Image.new(parent, config)
+
+	config = config or {}
+
+
+	local self =
+		setmetatable({}, Image)
+
+
+
+	self.Parent = parent
+
+
+	self.Image =
+		config.Image
+		or config.ImageId
+		or ""
+
+
+	self.Size =
+		config.Size
+		or UDim2.new(
+			0,
+			100,
+			0,
+			100
+		)
+
+
+	self.Transparency =
+		config.Transparency
+		or 0
+
+
+	self.Rotation =
+		config.Rotation
+		or 0
+
+
+	self.CornerRadius =
+		config.CornerRadius
+		or 0
+
+
+
+	self:_Create()
+
+
+
+	return self
+
+end
+
+
+
+
+
+-- ============================================================
+-- BUILD
+-- ============================================================
+
+function Image:_Create()
+
+
+	State:RegisterWidget(self)
+
+
+
+	local holder = Utils.Create(
+		"Frame",
+		{
+
+			Name = "ImageHolder",
+
+
+			BackgroundTransparency = 1,
+
+
+			Size = self.Size,
+
+
+			LayoutOrder =
+				#self.Parent:GetChildren()
+
+		}
+
+	)
+
+
+
+	holder.Parent =
+		self.Parent
+
+
+
+	self.Instance =
+		holder
+
+
+
+
+
+	local image = Utils.Create(
+		"ImageLabel",
+		{
+
+			Name = "Image",
+
+
+			Image =
+				self.Image,
+
+
+			BackgroundTransparency = 1,
+
+
+			Size =
+				UDim2.fromScale(
+					1,
+					1
+				),
+
+
+			Position =
+				UDim2.fromScale(
+					0,
+					0
+				),
+
+
+			ImageTransparency =
+				self.Transparency,
+
+
+			Rotation =
+				self.Rotation,
+
+
+			ScaleType =
+				Enum.ScaleType.Stretch,
+
+
+			BorderSizePixel = 0
+
+		}
+
+	)
+
+
+
+	image.Parent =
+		holder
+
+
+
+	self.ImageObject =
+		image
+
+
+
+
+	-- Optional rounded image
+
+	if self.CornerRadius > 0 then
+
+
+		local corner =
+			Utils.Create(
+				"UICorner",
+				{
+
+					CornerRadius =
+						UDim.new(
+							0,
+							self.CornerRadius
+						)
+
+				}
+
+			)
+
+
+		corner.Parent =
+			image
+
+
+		self.Corner =
+			corner
+
+
+	end
+
+
+
+end
+
+
+
+
+
+-- ============================================================
+-- METHODS
+-- ============================================================
+
+
+function Image:SetImage(id)
+
+
+	self.Image =
+		id
+
+
+	if self.ImageObject then
+
+		self.ImageObject.Image =
+			id
+
+	end
+
+
+end
+
+
+
+
+
+function Image:SetSize(size)
+
+
+	self.Size =
+		size
+
+
+	if self.Instance then
+
+		self.Instance.Size =
+			size
+
+	end
+
+
+end
+
+
+
+
+
+function Image:SetTransparency(value)
+
+
+	self.Transparency =
+		value
+
+
+	if self.ImageObject then
+
+		self.ImageObject.ImageTransparency =
+			value
+
+	end
+
+
+end
+
+
+
+
+
+function Image:SetRotation(value)
+
+
+	self.Rotation =
+		value
+
+
+	if self.ImageObject then
+
+		self.ImageObject.Rotation =
+			value
+
+	end
+
+
+end
+
+
+
+
+
+function Image:SetCornerRadius(value)
+
+
+	self.CornerRadius =
+		value
+
+
+	if self.Corner then
+
+		self.Corner.CornerRadius =
+			UDim.new(
+				0,
+				value
+			)
+
+	end
+
+
+end
+
+
+
+
+
+function Image:Destroy()
+
+
+	if self.Instance then
+
+		self.Instance:Destroy()
+
+	end
+
+
+	self.Instance = nil
+
+
+end
+
+
+
+
+
+return Image
